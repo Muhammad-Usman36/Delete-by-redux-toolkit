81691a345cf04ddb1d9f88cb49a7c81280e5953c
Muhammad Usman <mianusman3666@gmail.com>
2024-03-07 14:04:52 +0500

    debug

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
 import StudentList from './Redux/StudentList';
@@ -8,10 +8,12 @@ import StudentForm from './Redux/StudentForm';
 
 const App = () => {
   return (
+    <ScrollView>
       <Provider store={store}>
       <StudentList />
       <StudentForm/>
     </Provider>
+    </ScrollView>
   );
 };
 export default App;
